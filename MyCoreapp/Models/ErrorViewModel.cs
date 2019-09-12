3ffb34129051431d1a7453a8a04688f2040dfec9
Ajay Kumar Rudra <rudraajaykumar@gmail.com>
2019-09-12 06:46:49 +0530

    initial code

---
+++ MyCoreapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyCoreapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
